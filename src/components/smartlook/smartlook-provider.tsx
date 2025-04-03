159f11102e53e7164f7967f6c98a5567e1864c58
Dmytro Virych <inf@dmytrovirych.com>
2025-04-03 04:11:31 +0300

    feat: integrate Smartlook for user session tracking

--- src/components/smartlook/smartlook-provider.tsx
+++ src/components/smartlook/smartlook-provider.tsx
@@ -1,35 +1,33 @@
 'use client';
 
-import { useEffect } from 'react';
-
-declare global {
-    interface Window {
-        smartlook?: any;
-    }
-}
+import Smartlook from 'smartlook-client';
 
 export const SmartlookProvider = () => {
-    useEffect(() => {
-        if (typeof window !== 'undefined' && !window.smartlook) {
-            (function (d) {
-                const o = (window.smartlook = function (...args: any[]) {
-                    (o as any).api.push(args);
-                });
-                const h = d.getElementsByTagName('head')[0];
-                const c = d.createElement('script');
-                (o as any).api = [];
-                c.async = true;
-                c.type = 'text/javascript';
-                c.charset = 'utf-8';
-                c.src = 'https://web-sdk.smartlook.com/recorder.js';
-                h.appendChild(c);
-            })(document);
+    if (Smartlook.initialized() === false) {
+        Smartlook.init('4693cdebeca145c64b028916a6ff32a940f731b6');
+    }
 
-            window.smartlook('init', '4693cdebeca145c64b028916a6ff32a940f731b6', {
-                region: 'eu',
-            });
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined' && !window.smartlook) {
+    //         (function (d) {
+    //             const o = (window.smartlook = function (...args: any[]) {
+    //                 (o as any).api.push(args);
+    //             });
+    //             const h = d.getElementsByTagName('head')[0];
+    //             const c = d.createElement('script');
+    //             (o as any).api = [];
+    //             c.async = true;
+    //             c.type = 'text/javascript';
+    //             c.charset = 'utf-8';
+    //             c.src = 'https://web-sdk.smartlook.com/recorder.js';
+    //             h.appendChild(c);
+    //         })(document);
+
+    //         window.smartlook('init', '4693cdebeca145c64b028916a6ff32a940f731b6', {
+    //             region: 'eu',
+    //         });
+    //     }
+    // }, []);
 
     return null;
 };
