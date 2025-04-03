92b91dc7579421b76b66d631ea9f997eeab66b1c
Dmytro Virych <inf@dmytrovirych.com>
2025-04-03 04:18:16 +0300

    feat: lazy load Smartlook client initialization in SmartlookProvider

--- src/components/smartlook/smartlook-provider.tsx
+++ src/components/smartlook/smartlook-provider.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import Smartlook from 'smartlook-client';
+
+import { useEffect } from 'react';
 
 export const SmartlookProvider = () => {
-    if (Smartlook.initialized() === false) {
-        Smartlook.init('4693cdebeca145c64b028916a6ff32a940f731b6');
-    }
+    useEffect(() => {
+        (async () => {
+            if (typeof window === 'undefined') return;
+
+            const Smartlook = (await import('smartlook-client')).default;
+
+            if (!Smartlook.initialized()) {
+                Smartlook.init('4693cdebeca145c64b028916a6ff32a940f731b6', {
+                    region: 'eu',
+                });
+            }
+        })();
+    }, []);
 
     return null;
 };
